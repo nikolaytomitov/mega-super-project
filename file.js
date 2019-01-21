9e555f3a0016011c789913c8aef2ebd44833bddd
miki <niki@abv.bg>
2019-01-21 14:53:33 +0200

    promqna 2

--- file.js
+++ file.js
@@ -1,4 +1,6 @@
 console.log("Zashto trqbva da go uchim tova? Neshto po-interesno tolkoz li nqma?");
-console.log("toq shef moq da hodi da si gleda rabotata");
+console.log("toq mef moq da hodi da si gleda rabotata");
 
 console.log("Promqna 1");
+console.log("Promqna 2");
+console.log("Promqna 3");
