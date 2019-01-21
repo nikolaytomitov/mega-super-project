d6e8b087456d6882b2fe4ddc0a2a237124e003fb
miki <niki@abv.bg>
2019-01-21 14:37:36 +0200

    dasdasdas

--- mile.js
+++ mile.js
@@ -1,2 +1,5 @@
 console.log("Ebasi neverotqtna lekciq!");
-console.log("Kva e taq lekciq be bratche, nestho JS nqma li?")
+console.log("Kva e taq lekciq be bratche, nestho JS nqma li?");
+
+
+console.log("Toq moq shef e ebasi ...");
