6228f951cf4048c3632783f793abff92a0b2e5e6
miki <niki@abv.bg>
2019-01-21 14:41:36 +0200

    Revert "dasdasdas"
    This reverts commit d6e8b087456d6882b2fe4ddc0a2a237124e003fb.

--- mile.js
+++ mile.js
@@ -1,5 +1,2 @@
 console.log("Ebasi neverotqtna lekciq!");
-console.log("Kva e taq lekciq be bratche, nestho JS nqma li?");
-
-
-console.log("Toq moq shef e ebasi ...");
+console.log("Kva e taq lekciq be bratche, nestho JS nqma li?")
